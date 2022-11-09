549da001e19f96dde8721e669d231ee20a363c53
ANDREI KOCHANOV <andko4anov@yandex.ru>
2022-11-09 12:22:01 +0400

    test

---
+++ ytParsingBot.py
@@ -0,0 +1,38 @@
+import hashlib
+from youtube_search import YoutubeSearch
+from config import TOKEN
+from aiogram import Bot, types
+from aiogram.dispatcher import Dispatcher
+from aiogram.utils import executor
+
+
+def searcher(text):
+    res = YoutubeSearch(text, max_results=10).to_dict()
+    return res
+
+
+bot = Bot(token=TOKEN)
+dp = Dispatcher(bot)
+
+@dp.message_handler()
+async def start(message: types.Message):
+    await message.answer('Не надо мне писать, это инлайн бот!')
+
+
+@dp.inline_handler()
+async def inline_handler(query: types.InlineQuery):
+    text = query.query or 'echo'
+    links = searcher(text)
+
+    articles = [types.InlineQueryResultArticle(
+        id=hashlib.md5(f'{link["id"]}'.encode()).hexdigest(),
+        title=f'{link["title"]}',
+        url=f'https://www.youtube.com/watch?v={link["id"]}',
+        thumb_url=f'{link["thumbnails"][0]}',
+        input_message_content=types.InputTextMessageContent(
+            message_text=f'https://www.youtube.com/watch?v={link["id"]}'))
+        for link in links]
+    await query.answer(articles, cache_time=60, is_personal=True)
+
+
+executor.start_polling(dp, skip_updates=True)
